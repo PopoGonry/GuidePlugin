44cca43119171da7fda5a0cdc417f7ee457008b8
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2025-07-22 15:02:32 +0900

    2025-07-22
    프로젝트 생성

---
+++ src/main/java/com/popogonry/guidePlugin/GuidePlugin.java
@@ -0,0 +1,17 @@
+package com.popogonry.guidePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuidePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
